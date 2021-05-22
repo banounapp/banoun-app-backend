e2a1bae80756a5f2576bf3518284d018259238a6
OmarSaber-os <osaber218@gmail.com>
2021-05-22 02:29:35 +0200

    Login with facebook & facebook Auth

--- passports/FacebookPassport.js
+++ passports/FacebookPassport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: FacebookClientID,
       clientSecret: FacebookClientSecret,
-      callbackURL: "/facebook",
+      callbackURL: "/facebook/callback",
       profileFields: ["email", "name"],
     },
     function (accessToken, refreshToken, profile, done) {
