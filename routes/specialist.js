af2e6e7adce527483ef57f613d5c98fcfcc51b42
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-06-19 19:37:09 +0200

    email

--- routes/specialist.js
+++ routes/specialist.js
@@ -1,183 +1,179 @@
 const express = require("express");
 const Router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { body, validationResult } = require("express-validator");
 const config = require("config");
 const bycrpt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Specialist = require("../models/specialist");
 const nodemailer = require("../config/nodemailer.config");
-const auth =require('../middleware/auth');
-//for image 
-const crypto = require('crypto');
-const path = require('path')
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
+const auth = require("../middleware/auth");
+//for image
+const crypto = require("crypto");
+const path = require("path");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
 // const dbConnection =require('../config/db');
-const connection =require('../connection');
-const Appointment=require('../models/appointment');
+const connection = require("../connection");
+const Appointment = require("../models/appointment");
 
 const { equal } = require("assert");
 /////////////////////////////////////////////////////////////////////////////////
 const storage = new GridFsStorage({
-    url: "mongodb+srv://omar1234:omar@banoun.lrzmb.mongodb.net/main?retryWrites=true&w=majority",
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    bucketName: 'uploads'
-                };
-                resolve(fileInfo);
-            });
-        });
-    }
-})
+  url: "mongodb+srv://omar1234:omar@banoun.lrzmb.mongodb.net/main?retryWrites=true&w=majority",
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
 const upload = multer({ storage });
 let gfs;
-connection.once('open', () => {
-    gfs = Grid(connection.db, mongoose.mongo)
-    gfs.collection('uploads')
+connection.once("open", () => {
+  gfs = Grid(connection.db, mongoose.mongo);
+  gfs.collection("uploads");
 });
 //////////////////////////////////////////////////////////////////////////////////////////
 Router.post(
-    "/",
-    // upload.single('image'),
-    upload.array("image",4 ),
-    [
-      body("username", "username is required").not().isEmpty(),
-      body( "password","Please enter a password within 6 or more character"
-      ).isLength({ min: 6 }),
- ],
-    async (req, res) => {
-      //code
-  
-      const characters = config.get("Character");
-      let confirm = "";
-      for (let i = 0; i < 6; i++) {
-        confirm += characters[Math.floor(Math.random() * characters.length)];
-      }
-      // console.log(req.body);
-      console.log(confirm);
-  
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array(),
-        });
-      }
-      try {
-        // destructing Body => username , password
-  
-        const { username, password ,email,bio,gender,image,phone,job,city,
-          address,
-          Specialization} = req.body;
-  
-        // get user
-  
-        const checkUser = await Specialist.findOne({ username }).exec();
-  
-        // check if user already exists!
-        console.log(`check user is ${checkUser}`);
-  
-        if (checkUser) {
-          return res.status(500).json({ error: [`User Already Exists`] });
-        }
-  
-        //bycrypt password
-        let hashPassword = await bycrpt.hash(password, 10);
-  
-        // create user in DataBase
-        const specialist = await Specialist.create({
-          username,
-          password: hashPassword,
-          email,
-          bio,
-          NationalID:req.files[1],
-          gender,
-          phone,
-          job,
-          confirmationCode: confirm,
-          image:req.files[0],
-          certification:req.files[2],
-          city,
-          address,
-          Specialization
-        });
-  
-        // create a JWT Token
-        const secret = config.get("jwtSecret");
-  
-        const token = jwt.sign({ id: specialist._id }, secret, {
-          expiresIn: 360000,
-        });
-  
-        console.log(token);
-  
-        //  res.send({ token });
-        res.send({
-          message: "User was registered successfully! Please check your email",
-        });
-        nodemailer.sendConfirmationEmail(
-            specialist.username,
-            specialist.email,
-            specialist.confirmationCode
-        );
-      } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ errors: "Server Error" });
-      }
-    }
-  );
-//////////////////////////////////////////////////////////
-Router.get('/',async(req,res)=>{
+  "/",
+  // upload.single('image'),
+  upload.array("image", 4),
+  [
+    body("username", "username is required").not().isEmpty(),
+    body(
+      "password",
+      "Please enter a password within 6 or more character"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    //code
 
+    const characters = config.get("Character");
+    let confirm = "";
+    for (let i = 0; i < 6; i++) {
+      confirm += characters[Math.floor(Math.random() * characters.length)];
+    }
+    // console.log(req.body);
+    console.log(confirm);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
     try {
-    
-    
-        const specialist=await Specialist.find({statusjob: "approval"})
-    
-        res.json(specialist);
-        
-    } catch (err) {
-    
-        console.error(err.message);
-        res.status(500).send('Server Error');
-        
-    }
-    
-    
-    });
-/////////////////////////////////////////////////////////////////////////////////////
-Router.get('/:id',async(req,res)=>{
+      // destructing Body => username , password
 
-  try {
-  
-  
-      const specialist=await Specialist.find({_id:req.params.id})
-  
-      res.json(specialist);
-      
-  } catch (err) {
-  
+      const {
+        username,
+        password,
+        email,
+        bio,
+        gender,
+        image,
+        phone,
+        job,
+        city,
+        address,
+        Specialization,
+      } = req.body;
+
+      // get user
+
+      const checkUser = await Specialist.findOne({ username }).exec();
+
+      // check if user already exists!
+      console.log(`check user is ${checkUser}`);
+
+      if (checkUser) {
+        return res.status(500).json({ error: [`User Already Exists`] });
+      }
+
+      //bycrypt password
+      let hashPassword = await bycrpt.hash(password, 10);
+
+      // create user in DataBase
+      const specialist = await Specialist.create({
+        username,
+        password: hashPassword,
+        email,
+        bio,
+        NationalID: req.files[1],
+        gender,
+        phone,
+        job,
+        confirmationCode: confirm,
+        image: req.files[0],
+        certification: req.files[2],
+        city,
+        address,
+        Specialization,
+      });
+
+      // create a JWT Token
+      const secret = config.get("jwtSecret");
+
+      const token = jwt.sign({ id: specialist._id }, secret, {
+        expiresIn: 360000,
+      });
+
+      console.log(token);
+
+      //  res.send({ token });
+      res.send({
+        message: "User was registered successfully! Please check your email",
+      });
+      nodemailer.sendConfirmationEmail(
+        specialist.username,
+        specialist.email,
+        specialist.confirmationCode
+      );
+    } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
-      
+      res.status(500).json({ errors: "Server Error" });
+    }
   }
-  
-  
-  });
+);
+//////////////////////////////////////////////////////////
+Router.get("/", async (req, res) => {
+  try {
+    const specialist = await Specialist.find({ statusjob: "approval" });
+
+    res.json(specialist);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+/////////////////////////////////////////////////////////////////////////////////////
+Router.get("/:id", async (req, res) => {
+  try {
+    const specialist = await Specialist.find({ _id: req.params.id });
+
+    res.json(specialist);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////
 Router.post(
   "/login",
   [
-    body("email", "email is required").exists(),
+    body("username", "username is required").exists(),
     body("password", "Password is required").exists(),
   ],
   async (req, res) => {
@@ -190,11 +186,11 @@ Router.post(
     try {
       // destructing Body =>  username , password
 
-      const { email, password } = req.body;
+      const { username, password } = req.body;
 
       // get user
 
-      const checkUser = await Specialist.findOne({ email }).exec();
+      const checkUser = await Specialist.findOne({ username }).exec();
 
       // check if user already exists!
 
@@ -202,7 +198,7 @@ Router.post(
         return res.status(200).json({
           isSuccess: false,
           code: 1,
-          error: "Wrong email Or Password",
+          error: "Wrong username Or Password",
         });
       }
 
@@ -259,7 +255,9 @@ Router.get("/confirm/:confirmationCode", auth, async (req, res) => {
     .catch((e) => console.log("error", e));
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////
-Router.post("/schedule",auth,
+Router.post(
+  "/schedule",
+  auth,
   [
     body("date", "date is required").exists(),
     body("time", "time is required").exists(),
@@ -272,21 +270,19 @@ Router.post("/schedule",auth,
       });
     }
 
-    
     try {
       const { date, time } = req.body;
       // get specialist
       const appointment = await Appointment.create({
-        Specialist:req.signedId,
+        Specialist: req.signedId,
         date,
         time,
       });
 
-console.log(appointment);
+      console.log(appointment);
       const specialist = await Specialist.findById(req.signedId);
 
       // check if user specialist exists!
-
 
       if (!specialist) {
         return res.status(200).json({
@@ -296,15 +292,14 @@ console.log(appointment);
         });
       }
 
-    //   const newappointment={
-    //     date,
-    //     time
-    // }
-    
-    specialist.schedule.unshift(appointment._id);
-    await specialist.save();
-    res.json(specialist)
+      //   const newappointment={
+      //     date,
+      //     time
+      // }
 
+      specialist.schedule.unshift(appointment._id);
+      await specialist.save();
+      res.json(specialist);
     } catch (err) {
       console.error(err.message);
       res.status(500).json({ errors: "Server Error" });
@@ -314,67 +309,44 @@ console.log(appointment);
 
 ///////////////////////////////////////////////////////////////////////
 
-  
-Router.post('/edit',[
-  auth
-],
+Router.post(
+  "/edit",
+  [auth],
 
-async (req,res) =>{
+  async (req, res) => {
+    const { username, price, job, phone, age, city } = req.body;
 
+    //Build profile object
 
+    const profileFields = {};
+    // profileFields._id=req.signedId;
 
-const {
-username,
-price,
-job,
-phone,
-age,
-city,
+    if (username) profileFields.username = username;
+    if (price) profileFields.price = price;
+    if (phone) profileFields.phone = phone;
+    if (age) profileFields.age = age;
+    if (city) profileFields.city = city;
+    if (job) profileFields.job = job;
 
-}=req.body;
+    try {
+      let user = await Specialist.findOne({ _id: req.signedId });
 
-//Build profile object
+      if (user) {
+        user = await Specialist.findOneAndUpdate(
+          { _id: req.signedId },
 
-const profileFields={};
-// profileFields._id=req.signedId;
+          { $set: profileFields },
+          { new: true }
+        );
 
-if(username) profileFields.username=username;
-if(price) profileFields.price=price;
-if(phone) profileFields.phone=phone;
-if(age) profileFields.age=age;
-if(city) profileFields.city=city;
-if(job) profileFields.job=job;
+        res.json(user);
+      }
 
+      res.json("not found the Specialist");
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+);
 
-
-try{
-
-let user = await Specialist.findOne({_id:req.signedId});
-
-if(user){
-    
-    user=await Specialist.findOneAndUpdate(
-      {_id:req.signedId},
-   
-      {$set: profileFields},
-    { new:true }
-    );
-
-    res.json(user); 
-   }
-
-
-res.json("not found the Specialist")
-
-
-}catch(err){
-
-res.status(500).send(err);
-
-}
-
-});
-
-
-
-  module.exports=Router;
+module.exports = Router;
