548a4fb3e321eece52ffdb17312988dca268ce8f
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-06-22 20:14:28 +0200

    user

--- routes/specialist.js
+++ routes/specialist.js
@@ -355,5 +355,21 @@ Router.post(
     }
   }
 );
+////////////////////////////////////////////////////////////////////////
+
+Router.get("/", auth, async (req, res) => {
+  try {
+    const specialist = await Specialist.findOne({ _id: req.signedId });
+
+    if (!specialist || specialist.statusjob !== "approval") {
+      return res.status(400).json({ msg: "Not found the specialist" });
+    }
+
+    res.json(specialist);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = Router;
