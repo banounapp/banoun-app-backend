621217d368e0a30826cf5835d7bc0890742c2f1f
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-06-24 03:00:03 +0200

    event

--- models/specialist.js
+++ models/specialist.js
@@ -81,7 +81,12 @@ const specialistSchema = Schema({
       // },
     },
   ],
-
+  events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
   rate: {
     type: Number,
     default: 0,
@@ -90,6 +95,10 @@ const specialistSchema = Schema({
     type: String,
 
     default: "specialist",
+  },
+  eventnumber: {
+    type: Number,
+    default: 0,
   },
 });
 
